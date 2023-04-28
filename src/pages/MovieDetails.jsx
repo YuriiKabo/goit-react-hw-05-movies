e8a481aaa32b82a4b995d8bb659a3a6ad3dddce3
YuriiKabo <yourrocksmirnov@gmail.com>
2023-04-28 12:47:25 +0300

    add button

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
 import { Loader } from 'components/Loader';
 import { PageWrapper } from 'components/PageWrapper';
@@ -9,6 +9,7 @@ import Reviews from 'components/Reviews';
 import PropTypes from 'prop-types';
 
 const MovieDetails = () => {
+  const location = useLocation();
   const { isError, isLoading, movie } = useGetMovieDetail();
   if (isError)
     return (
@@ -18,35 +19,38 @@ const MovieDetails = () => {
 
   const { title, poster_path, vote_average, overview, genres } = movie;
   return (
-    <PageWrapper>
-      <Title content={title && title} />
-      {poster_path && (
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title && title}
-          width="200"
-        />
-      )}
-      <p>{`User Score:  ${(vote_average * 10).toFixed(0)}%`}</p>
-      <h3>Overview</h3>
-      <div> {overview && overview}</div>
-      <h3>Geners</h3>
-      {genres && genres.map(({ name, id }) => <span key={id}>{name} </span>)}
+    <>
+      <Link to={location.state?.from ?? '/'}>Go back</Link>
+      <PageWrapper>
+        <Title content={title && title} />
+        {poster_path && (
+          <img
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+            alt={title && title}
+            width="200"
+          />
+        )}
+        <p>{`User Score:  ${(vote_average * 10).toFixed(0)}%`}</p>
+        <h3>Overview</h3>
+        <div> {overview && overview}</div>
+        <h3>Geners</h3>
+        {genres && genres.map(({ name, id }) => <span key={id}>{name} </span>)}
 
-      <ul>
-        <li>
-          <Link to="cast" element={<Cast />}>
-            Cast
-          </Link>
-        </li>
-        <li>
-          <Link to="reviews" element={<Reviews />}>
-            Reviews
-          </Link>
-        </li>
-      </ul>
-      <Outlet />
-    </PageWrapper>
+        <ul>
+          <li>
+            <Link to="cast" element={<Cast />}>
+              Cast
+            </Link>
+          </li>
+          <li>
+            <Link to="reviews" element={<Reviews />}>
+              Reviews
+            </Link>
+          </li>
+        </ul>
+        <Outlet />
+      </PageWrapper>
+    </>
   );
 };
 
